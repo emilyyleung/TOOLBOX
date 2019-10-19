8d393360022f31835f33b4cd0aaa2fd3ed60a1f5
emilyyleung <30677901+emilyyleung@users.noreply.github.com>
2019-10-20 08:15:10 +1100

    Home Page Created!

--- home/views.py
+++ home/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+import json
+from . import data_tools
 
 # Create your views here.
+
+def home(request):	
+	print("HOME")
+	if request.method == 'GET':
+		# print(data_tools.data)
+		return render(request, "home/homepage.html", {"out": data_tools.data})
+	else:
+		pass
+	return render(request, "home/homepage.html", {"out": "working"})
